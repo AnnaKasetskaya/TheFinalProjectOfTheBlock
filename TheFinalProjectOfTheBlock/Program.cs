453f941986528b6b3bb935e23d9cc56650bc6698
Anna Kasetskaya <anna.kasetskaya@gmail.com>
2022-11-18 17:22:07 +0300

    added the main program TheFinalProjectOfTheBlock

---
+++ TheFinalProjectOfTheBlock/Program.cs
@@ -0,0 +1,58 @@
+﻿string[] CreateArray()
+{
+    Console.WriteLine("Введите количество элементов массива");
+    int size = Convert.ToInt32(Console.ReadLine());
+    string[] RandomArray = new string[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} элемент массива");
+        RandomArray[i] = Console.ReadLine();
+    }
+    return RandomArray;
+}
+
+
+void ShowArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+
+int GetArrayLength(string[] array)
+{
+    int length = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length < 4) length++;
+    }
+    return length;
+}
+
+
+string[] FindArrayElementsWithLengthLessFour(string[] array)
+{
+    int newArrayLen = GetArrayLength(array);
+    string[] newArray = new string[newArrayLen];
+    if (newArrayLen > 0)
+    {
+        int j = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i].Length < 4)
+            {
+                newArray[j] = array[i];
+                j++;
+            }
+        }
+    }
+    return newArray;
+}
+
+string[] array = CreateArray();
+ShowArray(array);
+Console.WriteLine();
+ShowArray(FindArrayElementsWithLengthLessFour(array));
